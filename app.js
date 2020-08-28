526af6b1027f32a2fe2245a410884097033a1277
yangxiaolu3 <yangxiaolu3@jd.com>
2020-08-28 18:28:10 +0800

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var ejs = require('ejs')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hashRouter = require('./routes/hash');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hash', hashRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
